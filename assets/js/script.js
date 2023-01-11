965f65777e82c38e8601a62863a8b1406316b1e6
Hohnn <jean-bateee@hotmail.fr>
2023-01-11 21:12:36 +0100

    Correction exo 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 /* Code ici, Bon courage ! */
+const allSteps = document.querySelectorAll('.steps > *') /* recupère tous les steps */
+
+allSteps.forEach( step => { /* execute une opération sur chaque step */
+    step.addEventListener('click', () => { /* ajoute un écoute d'événement */
+        resetAllSteps() /* appelle la function qui reset  */
+        step.classList.toggle('step-close') /* toggle add et remove automatiquement */
+    })
+})
+
+/* Referme tous les steps pour en avoir qu'un d'ouvert à la fois */
+function resetAllSteps() {
+    allSteps.forEach( step => {
+        step.classList.add('step-close')
+    })
+}
+
+/* Pour la transition c'est dans le css */
